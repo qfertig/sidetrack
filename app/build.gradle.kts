669a4adf2a3ffa3494d47e2fd32f204002b6e32d
Josh Taekman <josh.taekman@icloud.com>
2026-08-14 23:13:44 -0400

    release: v0.4.1
    - bump versionCode to 12, versionName to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sidespot.app"
         minSdk = 31
         targetSdk = 31
-        versionCode = 11
-        versionName = "0.4.0"
+        versionCode = 12
+        versionName = "0.4.1"
 
         // Only target arm64 (Sidephone SP-01 is aarch64)
         ndk {
